6e3da807cda8315c72f6fa270e18c16367d3125c
Moez Hudda <mhudda@twilio.com>
2022-10-17 16:55:13 -0700

    LTI support

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # BulkLookupV2
+# Bulk Lookups for Twilio Lookup
+
+1. Add TWILIO SID and AUTH TOKEN to demo.env
+1. Do you want to do `Line Type Intelligence` or `Sim Swap` Lookups? //currently only Line Type is supported in this Bulk Lookup Script
+    1. Put in the appropriate value.
+1. Execute npm install
+1. `source demo.env`
+1. Put source phone numbers into a csv file.  1 column of numbers. (e.g. 12024561111)
+1. Execute `node bulkLookup.js input.csv`
+
+## Output
+Found information:
+output-[type].csv
+
+Error information (invalid numbers):
+error-[type].csv
+
+### Resources
+The error output will include the error response for each particular number.
+You can find a list of all Twilio error codes, [here](https://www.twilio.com/docs/api/errors);
---
+++ bulkLookups.js
@@ -0,0 +1,110 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////
+////           Call script with: node bulkLookups.js input-file.csv               ////
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+var sid = process.env.BULK_SID;
+var auth = process.env.BULK_AUTH;
+var lookupType = process.env.BULK_LOOKUP_TYPE; // line type only right now
+var bulkCSVHeaders = process.env.BULK_CSV_HEADERS;
+var errorOutput = process.env.BULK_ERROR_OUTPUT;
+
+if (!(process.argv[2] && sid && auth && lookupType && bulkCSVHeaders)) {
+
+    console.log("Please pass in the input filename.");
+    process.exit(1);
+
+} else {
+    console.log("You are running " + lookupType + " Lookups");
+}
+
+var pnCsv = process.argv[2];
+
+var concurrency = 10;
+
+var request = require('request');
+var fs = require('fs');
+var async = require('async');
+var rp = require('request-promise');
+
+var parse = require('csv-parse');
+var transform = require('stream-transform');
+
+var stream = fs.createWriteStream('output-' + lookupType + '.csv');
+
+var errorStream = fs.createWriteStream('errors-' + lookupType + '.csv');
+
+var errorList = [];
+
+var uriSuffix = (lookupType === "line_type_intelligence") ? '?Fields=line_type_intelligence' : '?Type=caller-name';
+if (lookupType === "line_type_intelligence" && bulkCSVHeaders) {
+    stream.write("phone_number, carrier_name, carrier_type, mcc, mnc, country_code\n");
+} 
+
+var q = async.queue(function (task, callback) {
+
+    var uri = 'https://lookups.twilio.com/v2/PhoneNumbers/' + task.phoneNumber + uriSuffix;
+
+    var options = {
+        json: true,
+        uri: uri,
+        method: 'GET',
+        auth: {
+            user: sid,
+            pass: auth
+        }
+    };
+
+    rp(options)
+        .then(function (response) {
+
+
+            
+          if (lookupType === "line_type_intelligence") {
+                if (response.line_type_intelligence.carrier_name) {
+                    var carrierName = response.line_type_intelligence.carrier_name.replace(",", "");
+                }
+                stream.write(response.phone_number + ',' + response.line_type_intelligence.carrier_name + ',' + response.line_type_intelligence.type + ',' + response.line_type_intelligence.mobile_country_code + ',' + response.line_type_intelligence.mobile_network_code + ',' + response.country_code + '\n');
+            }
+            callback();
+
+        })
+        .catch(function (err) {
+            if (task)
+                if (errorOutput) {
+                    console.log('big error: ', err);
+                    stream.write("error" + '\n');
+                }
+            if (!task.retries) {
+                task.retries = 1;
+                q.push(task);
+            }
+            else if (task.retries < 5) {
+                task.retries++;
+                q.push(task);
+            } else {
+                console.log("error with this number: ", task.phoneNumber);
+                errorStream.write(task.phoneNumber + "invalid\n");
+                errorList.push([task, 'Maximum retries exceeded!']);
+            }
+
+            callback();
+        });
+
+}, concurrency);
+
+q.drain = function () {
+    console.log('All numbers looked up.');
+    if (errorList.length > 0 && errorOutput) {
+        if(errorOutput) {
+            console.log('Errors:\n', errorList);
+        }
+        console.log("Check errors-" + lookupType + ".csv for numbers and error codes");
+        console.log("See a list of errors codes at https://www.twilio.com/docs/api/errors");
+    }
+};
+
+fs.createReadStream(pnCsv)
+    .pipe(parse({delimiter: ':'}))
+    .on('data', function (csvrow) {
+        q.push({phoneNumber: csvrow[0]});
+    });
